9d3dcae325f555494ceec0385d290737ace0a505
Gabriel de Souza Climaco <gabrielsclimaco@gmail.com>
2018-09-21 10:50:20 -0300

    Add reducer for set balance action

---
+++ src/reducers/balance.js
@@ -0,0 +1,17 @@
+import * as constants from '../actions/constants';
+
+const balance = (state = 0, action) => {
+  let balance;
+
+  switch(action.type) {
+    case constants.SET_BALANCE:
+      balance = action.balance;
+      break;
+    default:
+      balance = state;
+  }
+
+  return balance;
+}
+
+export default balance;
