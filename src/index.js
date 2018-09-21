419803afb1fd3084cdcfa4a076228ff0c1d09dec
Gabriel de Souza Climaco <gabrielsclimaco@gmail.com>
2018-09-21 10:50:20 -0300

    Set up redux into app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import rootReducer from './reducers/balance';
+import { Provider } from 'react-redux';
 import App from './components/App';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={createStore(rootReducer)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
