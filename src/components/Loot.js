c2ed54e00ee7ebfe91d87dd680e27a8104c32755
Gabriel de Souza Climaco <gabrielsclimaco@gmail.com>
2018-10-05 10:50:22 -0300

    Fetches bitcoin on loot mount

--- src/components/Loot.js
+++ src/components/Loot.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { fetchBitcoin } from '../actions/bitcoin';
 
 export class Loot extends Component {
+  componentDidMount() {
+    this.props.fetchBitcoin();
+  }
+
   render() {
     return (
       <h3>Bitcoin balance</h3>
