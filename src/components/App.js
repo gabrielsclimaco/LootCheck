bb7f9c4eb315e12e2dc8517bed95acd6ceb45738
Gabriel de Souza Climaco <gabrielsclimaco@gmail.com>
2018-10-05 10:50:23 -0300

    Fix security issue

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const App = () => {
       <Wallet />
       <hr />
       <Loot />
-      <div>Powered by <a target='_blank' href='https://www.coindesk.com/price'>Coindesk</a></div>
+      <div>Powered by <a target='_blank' rel="noopener noreferrer" href='https://www.coindesk.com/price'>Coindesk</a></div>
     </div>
   )
 }
