4176d5bb76b4ba828a61db2c797c13a1d1f0325d
Gabriel de Souza Climaco <gabrielsclimaco@gmail.com>
2018-10-05 10:50:21 -0300

    Add test for cookie usage

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,12 +1,21 @@
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
-  const balance = 10;
+  describe('when initializing', () => {
+    const balance = 10;
 
-  it('sets a balance', () => {
-    expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
-      .toEqual(balance);
+    it('sets a balance', () => {
+      expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
+        .toEqual(balance);
+    });
+
+    describe('then re-initializing', () => {
+      it('reads the balance from cookies', () => {
+        expect(balanceReducer2(undefined, {})).toEqual(balance);
+      });
+    });
   });
 
   it('deposits into the balance', () => {
