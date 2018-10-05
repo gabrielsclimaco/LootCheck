9520c0b6f6fb96b10a40958355ccb57ad4a5e238
Gabriel de Souza Climaco <gabrielsclimaco@gmail.com>
2018-10-05 10:50:22 -0300

    Add test for displaying bitcoin

--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -22,4 +22,15 @@ describe('Loot', () => {
       expect(mockFetchbitcoin).toHaveBeenCalled();
     });
   });
+
+  describe('when there are valid bitcoin props', () => {
+    beforeEach(() => {
+      props = { balance: 10, bitcoin: { bpi: { USD: { rate: '1,000' } } } };
+      loot = shallow(<Loot {...props} />);
+    });
+
+    it('displays the correct bitcoin value', () => {
+      expect(loot.find('h3').text()).toEqual('Bitcoin balance: 0.01');
+    });
+  });
 });
